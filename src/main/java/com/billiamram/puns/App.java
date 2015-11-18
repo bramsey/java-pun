8dd5936060eb83f185a82144e36fc2a3cc614658
William Ramsey <pair+wramsey@pivotallabs.com>
2015-11-18 09:01:39 -0500

    Lowercase source files

--- src/main/java/com/billiamram/puns/App.java
+++ src/main/java/com/billiamram/puns/App.java
@@ -4,8 +4,8 @@ public class App {
   public static void main(String[] args) {
     new PunGenerator.Builder()
         .keyword("pun")
-        .minScore(250)
-        .minWordCount(5)
+        .minScore(260)
+        .minWordCount(3)
         .build()
         .generatePuns()
         .forEach(System.out::println);
